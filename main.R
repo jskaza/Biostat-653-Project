d98770ca43eda8e3310bf4bb01b379680d29828a
Tom Edwards <Tom Edwards>
2016-12-05 21:59:36 -0500

    added to background slide / created new model

--- main.R
+++ main.R
@@ -60,4 +60,11 @@ fit = MCMCglmm(cbind(attention_esense, meditation_esense) ~ trait - 1 + Gender +
          random = ~us(time):id, data = eeg_clean,
          family = c("gaussian", "gaussian"), rcov = ~us(trait):units)
 
-
+fit = MCMCglmm(cbind(attention_esense, meditation_esense) ~ trait - 1 +
+                 at.level(trait,1):Gender + at.level(trait,2):Gender+ 
+                 at.level(trait,1):Seen.video.before. +  at.level(trait,2):Seen.video.before.
+                 + at.level(trait,1):Saw.icons. +  at.level(trait,2):Saw.icons.
+                 + at.level(trait,1):Chosen.color + at.level(trait,2):Chosen.color
+                 + at.level(trait,1):time*as.factor(Session)+ at.level(trait,2):time*as.factor(Session),
+               random = ~us(time):id, data = eeg_clean,
+               family = c("gaussian", "gaussian"), rcov = ~us(trait):units)
